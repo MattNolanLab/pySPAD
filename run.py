dd25f911bfc27122ff7dff7844e2527d3d923358
kurtulusbulus <kurtulusbulus@gmail.com>
2022-10-18 16:14:48 +0100

    the first commit. all the files are added

---
+++ run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 15:14:34 2022
+
+@author: kurtulus
+"""
+
+import sys
+
+sys.path.append('config_files')
+
+from GUI_main import launch_GUI
+
+launch_GUI()
+
